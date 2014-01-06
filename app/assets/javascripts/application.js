10f06796452df19820147d02c62e42d264db3663
toshi <me@toshimaru.net>
2014-01-06 04:05:41 -0800

    Revert "Remove backbone demo"
    This reverts commit c8b4453e9ddd2fe4857b6681c2e6f56727186c4a.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require turbolinks
 //= require underscore
 //= require backbone
+//= require demo_app
 //= require_tree ../templates
 //= require_tree ./models
 //= require_tree ./collections
